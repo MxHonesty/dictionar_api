ebb20234e7ee5a705642dbf2677dbd2f7adbd8c4
Stelian <stoianstelian33@gmail.com>
2020-10-29 18:35:06 +0200

    endpoint for all words

--- dictionar/dex/serializers.py
+++ dictionar/dex/serializers.py
@@ -1,3 +1,22 @@
 """ Modulul care se ocupa cu transformarea modelelor din baza de date in 
     format json. """
 from rest_framework import serializers
+from .models import Cuvant, Definitie
+
+
+class DefinitieSerializer(serializers.HyperlinkedModelSerializer):
+    """ Clasa care specifica criteriul de formare a raspunsului JSON pentru
+        modelul Definitie. """
+    class Meta:
+        model = Definitie
+        fields = ["definitie_text"]
+
+
+class CuvantSerializer(serializers.HyperlinkedModelSerializer):
+    """ Clasa care specifica criteriul de formare a raspunsului JSON pentru
+        modelul Cuvant. """
+    definitii = DefinitieSerializer(source="definitie_set", many=True)
+    
+    class Meta:
+        model = Cuvant
+        fields = ["cuvant_text", "definitii"]
--- dictionar/dex/views.py
+++ dictionar/dex/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Cuvant, Definitie
+from .serializers import CuvantSerializer, DefinitieSerializer
 
-# Create your views here.
+
+class CuvantViewSet(viewsets.ModelViewSet):
+    """ API ENDPOINT care permite vizualizarea sau editarea cuvintelor. """
+    queryset = Cuvant.objects.all()
+    serializer_class = CuvantSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DefinitieViewSet(viewsets.ModelViewSet):
+    """ API ENDPOINT care permite vizualizarea sau editarea definitiilor. """
+    queryset = Cuvant.objects.all()
+    serializer_class = DefinitieSerializer
+    permission_classes = [permissions.IsAuthenticated]
